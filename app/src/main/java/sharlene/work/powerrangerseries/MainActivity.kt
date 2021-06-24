1fefa2668ddce0a358f1ab73d001dbd161fb7513
Crackmindedsk <shakumbhar@gmail.com>
2021-06-24 20:34:12 +0530

    Dialog alert

--- app/src/main/java/sharlene/work/powerrangerseries/MainActivity.kt
+++ app/src/main/java/sharlene/work/powerrangerseries/MainActivity.kt
@@ -3,6 +3,7 @@ package sharlene.work.powerrangerseries
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import sharlene.work.powerrangerseries.adapter.ItemAdapter
 import sharlene.work.powerrangerseries.data.Datasource
 
@@ -14,12 +15,21 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        showDialog()
         setContentView(R.layout.activity_main)
+
 
         val myData=Datasource().loadList()
         val recyclerView=findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter=ItemAdapter(this,myData)
         recyclerView.setHasFixedSize(true)
-
+    }
+    private fun showDialog(){
+     MaterialAlertDialogBuilder(this)
+         .setTitle("Instructions")
+         .setMessage("Click on image to find the season on youtube.\n\nClick on text to find about the show and cast.")
+         .setCancelable(true)
+         .setNegativeButton("OK"){_,_->this}
+         .show()
     }
 }
